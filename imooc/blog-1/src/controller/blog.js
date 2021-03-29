27cd82d5781b49259ebb168f2412481cba989f5a
jojo <zbhdaohuoxian@gmail.com>
2021-03-30 00:29:42 +0800

    初始化项目

---
+++ imooc/blog-1/src/controller/blog.js
@@ -0,0 +1,19 @@
+const getBlogList = (author, keyword) => {
+    return [{
+        id: 1,
+        title: "1title",
+        content: "aaaa",
+        author: "zhangsan",
+        createTime: 1617032185881
+    }, {
+        id: 2,
+        title: "2title",
+        content: "bbbb",
+        author: "zhangsan",
+        createTime: 1617032185881
+    }]
+}
+
+module.exports = {
+    getBlogList
+}
